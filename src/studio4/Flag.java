899e8f5f12a4d49a544d4ec94a8267f93ee11d95
Eli <Eli@L13-echamberlai.dhcp.wustl.edu>
2024-02-21 13:55:01 -0600

    switzerland

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,15 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.red);
+		StdDraw.filledSquare(0.5,0.5,0.2);
+		StdDraw.setPenColor(Color.black);
+		StdDraw.square(0.5,0.5,0.2);
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledRectangle(0.5,0.5,0.12,0.04);
+		StdDraw.filledRectangle(0.5,0.5,0.04,0.12);
+		StdDraw.filledPolygon(null, null);
+
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,51 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double one = in.nextDouble();
+		double two = in.nextDouble();
+		double three = in.nextDouble();
+		double four = in.nextDouble();
+		Color oewr = new Color(red, green, blue);
+		StdDraw.setPenColor(oewr);
+		if (shape.equals("ellipse")) {
+			if (filled) {
+				StdDraw.filledEllipse(one, two, three, four);
+				
+			}
+			else { 
+				StdDraw.ellipse(one, two, three, four);
+			}
+		}
+		else if (shape.equals( "rectangle")) {
+			if (filled) {
+				StdDraw.filledRectangle(one, two, three, four);
+				
+			}
+			else { 
+				StdDraw.rectangle(one, two, three, four);
+			}
+		}
+		/*else {
+			double five = in.nextDouble();
+			double six = in.nextDouble();
+			if (filled) {
+				StdDraw.line(one, two, three, four);
+				StdDraw.line(one, two, five, six);
+				StdDraw.line(five, six, three, four);
+				
+			}
+			else { 
+				StdDraw.line(one, two, three, four);
+				StdDraw.line(one, two, five, six);
+				StdDraw.line(five, six, three, four);
+			}
+
+		}
+		*/
 	}
 }
